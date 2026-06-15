dda0597caf1183bbc57e39cb799d927517e85393
Belladion83 <quitnp83@gmail.com>
2026-06-16 00:03:44 +0700

    Fix config syntax

--- js/config.js
+++ js/config.js
@@ -1,11 +1,1 @@
-window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.9-global-sync',
-  adminPassword: '832501',
-  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbxEytWyacmGPXntktLmicqxBmNtch2EER4OjKHDY06A/exec',
-  site: {
-    groomShortName: 'Phú Quí',
-    brideShortName: 'Ánh Nguyệt',
-    groomFullName: 'Trần Ngô Phú Quí',
-    brideFullName: 'Lương Thị Ánh Nguyệt',
-    groomRole: 'Trưởng Nam',
-    bride
+window.DEFAULT_WEDDING_CONFIG = {};
