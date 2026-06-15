34244141644e9cccada127f512696ff8519c3e7a
Belladion83 <quitnp83@gmail.com>
2026-06-16 00:17:36 +0700

    Fix complete default config

--- js/config.js
+++ js/config.js
@@ -1,1 +1,3 @@
-window.DEFAULT_WEDDING_CONFIG={version:'1.10',adminPassword:'832501',googleAppsScriptUrl:'https://script.google.com/macros/s/AKfycbxEytWyacmGPXntktLmicqxBmNtch2EER4OjKHDY06A/exec',site:{groomShortName:'Phu Qui',brideShortName:'Anh Nguyet',groomFullName:'Tran Ngo Phu Qui',brideFullName:'Luong Thi Anh Nguyet',groomRole:'Truong Nam',brideRole:'Truong Nu',weddingDate:'2026-07-18T18:00:00+07:00',displayDate:'18 . 07 . 2026',eventType:'Tiec bao hy',saveText:'SAVE OUR
+window.DEFAULT_WEDDING_CONFIG={
+version:'1.10',adminPassword:'832501',googleAppsScriptUrl:'https://script.google.com/macros/s/AKfycbxEytWyacmGPXntktLmicqxBmNtch2EER4OjKHDY06A/exec',
+site:{groomShortName:'Phu Qui',brideShortName:'Anh Nguyet',groomFullName:'Tran Ngo Phu Qui',brideFullName:'Luong Thi Anh Nguyet',groomRole:'Truong Nam',brideRole:'Truong Nu',weddingDate:'2026-07-18T18:00:00+07:00',displayDate:'18 . 07 . 2026
