4549b8e08d2cb2a425865a445908000073524c7b
Belladion83 <quitnp83@gmail.com>
2026-06-16 00:04:26 +0700

    Restore synced wedding config

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
-window.DEFAULT_WEDDING_CONFIG = {};
+window.DEFAULT_WEDDING_CONFIG = {
+  version: '1.10-global-sync-url',
+  adminPassword: '832501',
+  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbxEytWyacmGPXntktLmicqxBmNtch2EER4OjKHDY06A/exec',
+  site: {
+    groomShortName: 'Phú Quí',
+    brideShortName: 'Ánh Nguyệt',
+    groomFullName: 'Trần Ngô Phú Quí',
+    brideFullName: 'Lương Thị Ánh Nguyệt',
+    groomRole: 'Trưởng Nam',
+    brideRole: 'Trưởng Nữ',
