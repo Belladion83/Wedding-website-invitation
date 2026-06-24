ebf2965fb7ea1446890fb293ae2832810c49ada1
Belladion83 <quitnp83@gmail.com>
2026-06-24 21:27:30 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.29-bg-text-flap-fix',
+  version: '1.30-text-outside-envelope',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
