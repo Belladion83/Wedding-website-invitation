c716d7ddc132bfa48604cbd2a56dbdbd767f4a68
Belladion83 <quitnp83@gmail.com>
2026-06-25 13:00:07 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.33-envelope-style-reference',
+  version: '1.34-triangular-envelope-only',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
