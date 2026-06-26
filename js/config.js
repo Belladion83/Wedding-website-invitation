39b07ea901832b026c828f04f416daea90d887f9
Belladion83 <quitnp83@gmail.com>
2026-06-26 16:54:15 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.37-dresscode-colors-calendar-center',
+  version: '1.38-section-alignment-fix',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
