61e6813815e9657c430302e28596a2f06af03dc6
Belladion83 <quitnp83@gmail.com>
2026-06-27 23:52:51 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.46-keep-section-size-smaller-text',
+  version: '1.47-hero-full-width-restored',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
