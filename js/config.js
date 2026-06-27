53b4c97e1b2ed5348699b05f7520e73b448f0301
Belladion83 <quitnp83@gmail.com>
2026-06-28 00:35:12 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.52-calendar-heart-mark',
+  version: '1.53-calendar-heart-bigger-softer',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
