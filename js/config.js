3a4a4d2dce5a079722141e27422b51528e9bb74b
Belladion83 <quitnp83@gmail.com>
2026-06-27 23:38:40 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.43-gold-text-better-font-no-caps',
+  version: '1.44-full-names-one-line',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
