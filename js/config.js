b2ee10deab46b930ad50bb92b740a4d5d1996beb
Belladion83 <quitnp83@gmail.com>
2026-06-28 16:07:48 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.DEFAULT_WEDDING_CONFIG = {
-  version: '1.71-calendar-titles-thanks-fix',
+  version: '1.73-title-double-star-fix',
   adminPassword: '832501',
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwwCsbun86MMYmFgb3D9-oyhfSsc2wBeaT15fOXndcOBX01eoAyztkIMp2VAe0U2kGCDQ/exec',
   site: {
